095091d78a8c66b3ef073a69d043f54b14f19816
Personal <personal@Sehrans-MacBook-Station.local>
2024-09-24 21:04:29 +0800

    Update

---
+++ js/portfolio.js
@@ -0,0 +1,45 @@
+const portfolioList = {
+    "corporate": [
+        {
+            "title": "Guinness Malaysia - St. Patrick's Day 2024",
+            "description": "Done for advertisement.",
+            "workby": "DOP: thattudio",
+            "poster": "https://cdn-prod-ccv.adobe.com/DkbWp_igM3m/image/DkbWp_igM3m_poster.jpg?hdnts=st%3D1713359243%7Eexp%3D1713618443%7Eacl%3D%2Fshared_assets%2Fimage%2F*%21%2Fz%2FDkbWp_igM3m%2Frend%2F*%21%2Fi%2FDkbWp_igM3m%2Frend%2F*%21%2FDkbWp_igM3m%2Frend%2F*%21%2FDkbWp_igM3m%2Fimage%2F*%21%2FDkbWp_igM3m%2Fcaptions%2F*%7Ehmac%3D405d11820ca4f9ca072a28ef7f5f72d3213cf04c90aa5a339d2480b790e9f194",
+            "video": "https://cdn-prod-ccv.adobe.com/DkbWp_igM3m/rend/DkbWp_igM3m_576.mp4?hdnts=st%3D1713312794%7Eexp%3D1713571994%7Eacl%3D%2Fshared_assets%2Fimage%2F*%21%2Fz%2FDkbWp_igM3m%2Frend%2F*%21%2Fi%2FDkbWp_igM3m%2Frend%2F*%21%2FDkbWp_igM3m%2Frend%2F*%21%2FDkbWp_igM3m%2Fimage%2F*%21%2FDkbWp_igM3m%2Fcaptions%2F*%7Ehmac%3Df69025ef422b8e23226eb0807633454ce4251b4f36aafcc461c187fd1278470f",
+            "orientation": "portrait",
+            "date": "Novemeber, 2023"
+        },
+        {
+            "title": "Lot 168 Selayang - The White Knight",
+            "description": "",
+            "workby": "DOP: thattudio",
+            "poster": "https://cdn-prod-ccv.adobe.com/5dn5iRoKYI9/image/5dn5iRoKYI9_poster.jpg?hdnts=st%3D1713313999%7Eexp%3D1713573199%7Eacl%3D%2Fshared_assets%2Fimage%2F*%21%2Fz%2F5dn5iRoKYI9%2Frend%2F*%21%2Fi%2F5dn5iRoKYI9%2Frend%2F*%21%2F5dn5iRoKYI9%2Frend%2F*%21%2F5dn5iRoKYI9%2Fimage%2F*%21%2F5dn5iRoKYI9%2Fcaptions%2F*%7Ehmac%3D063f8ff26d9a0d6cc9ab826e00e2efc40f0e92ba127168ff909be9b091e5af4f",
+            "video": "https://cdn-prod-ccv.adobe.com/5dn5iRoKYI9/rend/5dn5iRoKYI9_576.mp4?hdnts=st%3D1713312794%7Eexp%3D1713571994%7Eacl%3D%2Fshared_assets%2Fimage%2F*%21%2Fz%2F5dn5iRoKYI9%2Frend%2F*%21%2Fi%2F5dn5iRoKYI9%2Frend%2F*%21%2F5dn5iRoKYI9%2Frend%2F*%21%2F5dn5iRoKYI9%2Fimage%2F*%21%2F5dn5iRoKYI9%2Fcaptions%2F*%7Ehmac%3Dd2ab74825b322be9e3aa74453bf3eb21ca24ba652d1a30f2cda224354e48a9a1",
+            "orientation": "landscape",
+            "date": "October, 2023"
+        }
+
+    ],
+    "food_and_beverages": [
+        {
+            "title": "Moses Wong x Carlsberg",
+            "description": "",
+            "workby": "DOP: thattudio",
+            "poster": "https://cdn-prod-ccv.adobe.com/McPWQSI3XwI/image/McPWQSI3XwI_poster.jpg?hdnts=st%3D1713365079%7Eexp%3D1713624279%7Eacl%3D%2Fshared_assets%2Fimage%2F*%21%2Fz%2FMcPWQSI3XwI%2Frend%2F*%21%2Fi%2FMcPWQSI3XwI%2Frend%2F*%21%2FMcPWQSI3XwI%2Frend%2F*%21%2FMcPWQSI3XwI%2Fimage%2F*%21%2FMcPWQSI3XwI%2Fcaptions%2F*%7Ehmac%3D0eae808814435b43390034b734228b52191e359f8ce70f6bbebcfb25cacc09c2",
+            "video": "https://cdn-prod-ccv.adobe.com/McPWQSI3XwI/rend/McPWQSI3XwI_576.mp4?hdnts=st%3D1713365079%7Eexp%3D1713624279%7Eacl%3D%2Fshared_assets%2Fimage%2F*%21%2Fz%2FMcPWQSI3XwI%2Frend%2F*%21%2Fi%2FMcPWQSI3XwI%2Frend%2F*%21%2FMcPWQSI3XwI%2Frend%2F*%21%2FMcPWQSI3XwI%2Fimage%2F*%21%2FMcPWQSI3XwI%2Fcaptions%2F*%7Ehmac%3D0eae808814435b43390034b734228b52191e359f8ce70f6bbebcfb25cacc09c2",
+            "orientation": "portrait",
+            "date": "January, 2024"
+        }
+    ],
+    "music_video": [
+        {
+            "title": "Mrs Malaysia Globe - Rehearsal Video",
+            "description": "",
+            "workby": "DOP: thattudio",
+            "poster": "https://cdn-prod-ccv.adobe.com/FaTywVZXXK2/image/FaTywVZXXK2_poster.jpg?hdnts=st%3D1713365409%7Eexp%3D1713624609%7Eacl%3D%2Fshared_assets%2Fimage%2F*%21%2Fz%2FFaTywVZXXK2%2Frend%2F*%21%2Fi%2FFaTywVZXXK2%2Frend%2F*%21%2FFaTywVZXXK2%2Frend%2F*%21%2FFaTywVZXXK2%2Fimage%2F*%21%2FFaTywVZXXK2%2Fcaptions%2F*%7Ehmac%3Dc815cbf7f9fc8a7eae69d9c4b0044638c204aa5c349a57ce1510c46bf8c7286b",
+            "video": "https://cdn-prod-ccv.adobe.com/FaTywVZXXK2/rend/FaTywVZXXK2_576.mp4?hdnts=st%3D1713365409%7Eexp%3D1713624609%7Eacl%3D%2Fshared_assets%2Fimage%2F*%21%2Fz%2FFaTywVZXXK2%2Frend%2F*%21%2Fi%2FFaTywVZXXK2%2Frend%2F*%21%2FFaTywVZXXK2%2Frend%2F*%21%2FFaTywVZXXK2%2Fimage%2F*%21%2FFaTywVZXXK2%2Fcaptions%2F*%7Ehmac%3Dc815cbf7f9fc8a7eae69d9c4b0044638c204aa5c349a57ce1510c46bf8c7286b",
+            "orientation": "landscape",
+            "date": "January, 2024"
+        }
+    ]
+}
